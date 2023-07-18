2a947defe36a8a3266ea129348a2c0841f204fba
--remove-section <anuragChaturvedi978@gmail.com>
2023-07-18 19:04:17 +0530

    feat: fuzz testing

--- hello_foundry/test/Counter.t.sol
+++ hello_foundry/test/Counter.t.sol
@@ -6,16 +6,18 @@ import "../src/Counter.sol";
 import "forge-std/console.sol";
 
 contract CounterTest is Test {
+    uint256 sepoliaFork;
     Counter public counter;
-
+    string SEPOLIA_URL = vm.envString("SEPOLIA_URL");
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+        sepoliaFork = vm.createFork(SEPOLIA_URL);
     }
 
     function testIncrement() public {
         counter.increment();
-        console.log(counter.number());
+        console.log(SEPOLIA_URL);
         assertEq(counter.number(), 1);
     }
 
